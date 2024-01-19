1257585e5e5db78bbc57e79bc4ec8b80440a674a
maruf-ahmed <marufahamed1110@gmail.com>
2024-01-20 02:25:57 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    colors: {
+        'blue': '#1fb6ff',
+        'nav-bg-color':'#0E1133',
+      },
+    plugins: [],
+  }
